a9efce3c6abdedaf1b1a3a8408a6ea32aa783e34
liubob98 <liubob98@gmail.com>
2015-04-02 15:18:57 -0500

    Driver

--- src/Driver.java
+++ src/Driver.java
@@ -1,4 +1,11 @@
+import javax.swing.SwingUtilities;
+
 
 public class Driver {
-
+	
+	public static void main(String[] args) //the main method
+	{
+		SwingUtilities.invokeLater(new Runnable() {
+		});
+	}
 }
