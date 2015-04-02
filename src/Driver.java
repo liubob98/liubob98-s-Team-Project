870aee24f06af2f36b741b50ad3ce73ac67a20c3
liubob98 <liubob98@gmail.com>
2015-04-02 15:43:48 -0500

    MORE

--- src/Driver.java
+++ src/Driver.java
@@ -3,9 +3,10 @@ import javax.swing.SwingUtilities;
 
 public class Driver {
 	
-	public static void main(String[] args) //the main method
+	public static void main(String[] args)
 	{
 		SwingUtilities.invokeLater(new Runnable() {
+			public void run() { new FrontEnd(); }
 		});
 	}
 }
--- src/Encoder.java
+++ src/Encoder.java
@@ -1,4 +1,26 @@
+import java.io.File;
+import java.util.*;
 
 public class Encoder {
+	
+	private File file;
+	
+	
+	public Encoder(String filename){
+		file = new File(filename); 
+	}
+	
+	public void write(String plaintext){
+		
+	}
+	
+	public List<String> read(){
+		return new ArrayList();
+	}
+	
+	
+	
+	
+	
 
 }
