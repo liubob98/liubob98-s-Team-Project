cf275c558a2d49d7e6e5f8c7b9949e0717764b73
liubob98 <liubob98@gmail.com>
2015-04-09 15:18:57 -0500

    NEWEST CHANGES
    encoder.

--- src/Encoder.java
+++ src/Encoder.java
@@ -1,26 +1,57 @@
-import java.io.File;
 import java.util.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Scanner;
 
-public class Encoder {
-	
+
+public class Encoder
+{
 	private File file;
 	
-	
 	public Encoder(String filename){
-		file = new File(filename); 
+		file = new File(filename);
 	}
 	
-	public void write(String plaintext){
+	public void write(String plaintext)
+	{
+		try
+		{
+			PrintWriter writer = new PrintWriter(file);
+			for(char x : plaintext.toCharArray()){
+				writer.write((char)(x - 350));
+			}
+			writer.append("\n");
+			writer.close();
+		}
+		catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public ArrayList<String> read()
+	{
+		try
+		{
+			Scanner scan = new Scanner(file);
+			ArrayList<String> arrlist = new ArrayList<String>();
+			while(scan.hasNextLine()){
+				String s = scan.nextLine();
+				String p = "";
+				for(char x : s.toCharArray())
+				{
+					p += (char)(x + 350);
+				}
+				arrlist.add(p);
+			}
+			scan.close();
+			return arrlist;
+		}
+		catch (FileNotFoundException swag){
+			swag.printStackTrace();
+		}
 		
+		return null;
 	}
-	
-	public List<String> read(){
-		return new ArrayList();
-	}
-	
-	
-	
-	
-	
-
 }
